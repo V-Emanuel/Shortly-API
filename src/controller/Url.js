c383aceac9e06f33272ae6fecc8860094a1063d4
V-Emanuel <gvictor567@gmail.com>
2023-03-03 13:13:41 -0300

    fix: return short url format

--- src/controller/Url.js
+++ src/controller/Url.js
@@ -14,16 +14,20 @@ export async function urlShorten(req, res){
     try {
         await db.query(`INSERT INTO urls ("userId", url, "shortUrl") VALUES ($1, $2, $3)`,
         [session.userId, url, shortUrl]);
-        res.sendStatus(201);
+        const returnUrl = await db.query(`SELECT id, "shortUrl" FROM urls WHERE "shortUrl" = $1`, [shortUrl]);
+        res.status(201).send(returnUrl.rows[0]);
     } catch (error) {
         res.status(500).send(error.message);
     }
 }
 
+
+
 export async function getUrlId(req, res){
     const {id} = req.params;
     try {
         const url = await db.query(`SELECT id, "shortUrl", url FROM urls WHERE id = $1`, [id]);
+        console.log(url.rows)
         res.send(url.rows[0]);
     } catch (error) {
         res.status(500).send(error.message);
