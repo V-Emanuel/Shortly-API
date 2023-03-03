dca95f2fed87b78cf26fdb49a4c28a0feaceda80
V-Emanuel <gvictor567@gmail.com>
2023-03-03 13:52:52 -0300

    fix: try again

--- src/controller/Auth.js
+++ src/controller/Auth.js
@@ -24,7 +24,7 @@ export async function signUp(req, res) {
 }
 
 export async function signIn(req, res) {
-    const user = req.body;
+   {/* const user = req.body;
     const validation = signInSchema.validate(user, { abortEarly: true });
     if (validation.error) {
         const errors = validation.error.details.map((detail) => detail.message);
@@ -44,6 +44,21 @@ export async function signIn(req, res) {
         res.sendStatus(200)
     } catch (error) {
         res.status(500).send(error);
+    }*/}
+    const { email, password } = req.body
+    const user = await db.query(`SELECT * FROM users WHERE email = $1;`, [email])
+
+    if (user.rowCount === 0 || user.rows[0].senha !== password) {
+        return res.sendStatus(401)
+    }
+
+    try {
+        const token = uuidV4()
+        await db.query(`INSERT INTO sessions ("userId", token)
+        VALUES ($1, $2);`, [user.rows[0].id, token])
+        return res.status(200).send({ token })
+    } catch (error) {
+        res.send(error)
     }
 
 }
